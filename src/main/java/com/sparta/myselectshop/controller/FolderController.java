70f8c95a8b49eb82ee236d1e8e2d1bce85e4c15b
yuio7279 <yuio7279@gmail.com>
2023-07-17 17:56:49 +0900

    ControllerAdvice 적용

--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -30,14 +30,5 @@ public class FolderController {
         return folderService.getFolders(userDetails.getUser());
 
     }
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.BAD_REQUEST
-        );
-    }
+
 }
